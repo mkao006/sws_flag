fe4c951ed9047a4387ec4f9dcd35b1bcb36405bc
rockclimber112358 <rockclimber112358@gmail.com>
2015-03-13 14:50:35 +0100

    Added documentation and input checks

--- codes/aggregateObservationFlag.R
+++ codes/aggregateObservationFlag.R
@@ -1,17 +1,26 @@
-##' This function performs aggregation on the flag
+##' This function performs aggregation of flags
 ##'
 ##' The aggregation is based on the weight of the flag as designated
-##' by the table. Official sources has high weight while data from
-##' unreliable sources has low weight.
+##' by the table. Official sources have high weight while data from
+##' unreliable sources have low weight.
 ##'
-##' @param ... Vectors of Flags, see detail
+##' @param ... Vectors of flags, each of the same length.  See details.
 ##' @param flagTable The table which maps the relationship between the
 ##' flag and its information weight.
 ##'
-##' @details The arguement for flag is the same to the function
-##' sum. Multiple instances of the flag can be pass in to the function
-##' to be aggregated.
-##'
+##' @details This function works in a similar way to base::sum.  The first
+##' argument is ..., and this allows the user to pass multiple vectors of
+##' flags which should all be aggregated.
+##' 
+##' @examples
+##' aggregateObservationFlag("T", "E")
+##' # Passing multiple flags aggregates them all together:
+##' aggregateObservationFlag("", "T", "E", "I", "M")
+##' # You can also pass vectors:
+##' aggregateObservationFlag(c("T", "T"), c("M", ""))
+##' # And more than 2 vectors:
+##' aggregateObservationFlag(c("T", "T"), c("M", ""), c("I", ""))
+##' 
 ##' @seealso \code{\link{sum}}
 ##'
 ##' @export
@@ -20,6 +29,14 @@
 aggregateObservationFlag = function(..., flagTable = faoswsFlagTable){
     
     flags = list(...)
+
+    ## Data Quality Checks
+    flagLengths = lapply(flags, length)
+    if(any(flagLengths != flagLengths[[1]]))
+        stop("All input flag vectors must be of the same length.")
+    allFlags = do.call("c", flags)
+    if(any(!allFlags %in% flagTable$flagObservationStatus))
+        stop("Some flags are not in the flagTable.")
     
     ## Convert flags to numeric information weight as determined by
     ## the table.
--- codes/weight2flag.R
+++ codes/weight2flag.R
@@ -1,14 +1,15 @@
 ##' Function to convert information weight to flag.
 ##'
-##' The function converts a numeric information weight back to flags.
+##' The function converts a numeric information weight back to a flag.
 ##'
-##' @param flagObservationWeight The flag information weight
-##' @param flagTable The table mapping the flag and weights
+##' @param flagObservationWeight The information weight of an observation.
+##' @param flagTable The table mapping flags and weights.
 ##'
 ##' @export
+##' 
 
 weight2flag = function(flagObservationWeight,
                        flagTable = faoswsFlagTable){
-    index = match(flagObservationWeight, flagTable$flagObservationWeight)
+    index = match(flagObservationWeight, flagTable$flagObservationWeights)
     as.character(flagTable$flagObservationStatus[index])
 }
