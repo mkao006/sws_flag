79ff347ed26415e4129bb52a4166c73122321aff
mkao006 <mkao006@gmail.com>
2015-07-22 16:35:10 -0600

    added the degenerate distribution

--- R/parameterise.R
+++ R/parameterise.R
@@ -11,57 +11,62 @@
 ##' @export
 
 parameterise = function(obsValue, selfInformation, distribution){
-    switch(distribution,
-           `normal` = {
-               mean = obsValue               
-               sd = sqrt(exp(2 * selfInformation)/(2 * pi * exp(1)))
-               list(mean = mean, sd = sd)
-           },
-           `cauchy` = {
-               location = obsValue
-               scale = exp(selfInformation - log(4 * pi))
-               list(location = location, scale = scale)
-           },
-           `truncNorm` = {
-               ## The truncated normal here is defined as having lower
-               ## bound of zero and upper bound of infinity. That is,
-               ## on the positive real line.
-               mean = obsValue
-               sd = uniroot(
-                   function(x){
-                       entropyTruncNormal(a = 0, sd = x, mean = obsValue) -
-                           selfInformation
-                   },
-                   interval = c(1e-20, 1e20))$root
-               list(mean = mean, sd = sd)
-           },
-           `logNorm` = {
-               sdlog = uniroot(
-                   function(x){
-                       ## The mean has to be computed first, as the
-                       ## observed value assumed to be the mode of the
-                       ## distribution.
-                       mean = log(obsValue) + x^2
-                       entropyLogNorm(mean = mean, sd = x) -
-                           selfInformation
-                   },
-                   interval = c(1e-50, 1e20))$root
-               meanlog = log(obsValue) + sdlog^2
-               list(meanlog = meanlog, sdlog = sdlog)
-           },
-           `exponential` = {
-               rate = exp(-(selfInformation - 1))
-               warning("Exponential distribution has mode at zero")
-               list(rate = rate)
-           },
-           `weibull` = {
-               shape = uniroot(
-                   function(x){
-                       scale = obsValue/((x - 1/x)^1/x)
-                       entropyWeibull(shape = x, scale = scale) - selfInformation
-                   }, interval = c(1 + 1e-50, 1e20))$root
-               scale = scale = obsValue/((shape - 1/shape)^1/shape)
-               list(shape = shape, scale = scale)
-           }
-           )
-}       
+    if(selfInformation == 0){
+        list(k0 = obsValue)
+    } else {
+        switch(distribution,
+               `normal` = {
+                   mean = obsValue               
+                   sd = sqrt(exp(2 * selfInformation)/(2 * pi * exp(1)))
+                   list(mean = mean, sd = sd)
+               },
+               `cauchy` = {
+                   location = obsValue
+                   scale = exp(selfInformation - log(4 * pi))
+                   list(location = location, scale = scale)
+               },
+               `truncNorm` = {
+                   ## The truncated normal here is defined as having lower
+                   ## bound of zero and upper bound of infinity. That is,
+                   ## on the positive real line.
+                   mean = obsValue
+                   sd = uniroot(
+                       function(x){
+                           entropyTruncNormal(a = 0, sd = x, mean = obsValue) -
+                               selfInformation
+                       },
+                       interval = c(1e-20, 1e20))$root
+                   list(mean = mean, sd = sd)
+               },
+               `logNorm` = {
+                   sdlog = uniroot(
+                       function(x){
+                           ## The mean has to be computed first, as the
+                           ## observed value assumed to be the mode of the
+                           ## distribution.
+                           mean = log(obsValue) + x^2
+                           entropyLogNorm(mean = mean, sd = x) -
+                               selfInformation
+                       },
+                       interval = c(1e-50, 1e20))$root
+                   meanlog = log(obsValue) + sdlog^2
+                   list(meanlog = meanlog, sdlog = sdlog)
+               },
+               `exponential` = {
+                   rate = exp(-(selfInformation - 1))
+                   warning("Exponential distribution has mode at zero")
+                   list(rate = rate)
+               },
+               `weibull` = {
+                   shape = uniroot(
+                       function(x){
+                           scale = obsValue/((x - 1/x)^1/x)
+                           entropyWeibull(shape = x, scale = scale) -
+                               selfInformation
+                       }, interval = c(1 + 1e-50, 1e20))$root
+                   scale = scale = obsValue/((shape - 1/shape)^1/shape)
+                   list(shape = shape, scale = scale)
+               }
+               )
+    }
+}
